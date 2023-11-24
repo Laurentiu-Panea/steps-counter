28448871780326e95197dc69a037fd39186f34d5
Laurentiu-Panea <151642528+Laurentiu-Panea@users.noreply.github.com>
2023-11-24 20:53:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  function handleClick() {
+    setStep(1);
+    setCount(0);
+  }
+
+  return (
+    <div>
+      <div>
+        <span>
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={step}
+            onChange={(e) => setStep(Number(e.target.value))}
+          />
+          Step:{step}
+        </span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {(step !== 1 || count !== 0) && (
+        <div>
+          <button onClick={handleClick}>Reset</button>
+        </div>
+      )}
+    </div>
+  );
+}
+//`{step>1&&count>0?"":hidden}`
